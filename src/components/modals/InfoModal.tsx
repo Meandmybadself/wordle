07666b89e2d4a94295f0da561c9265c894b0bebd
Jeffery Bennett <meandmybadself@gmail.com>
2024-11-06 11:14:21 -0600

    Introduces

--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 export const InfoModal = ({ isOpen, handleClose }: Props) => {
   return (
-    <BaseModal title="Happy Birthday!" isOpen={isOpen} handleClose={handleClose}>
+    <BaseModal title="Wordle" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500 dark:text-gray-300">
         You should know how to play this game by now, but just in case:
         Guess the word in 6 tries. After each guess, the color of the tiles will
@@ -19,44 +19,18 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
         <Cell
           isRevealing={true}
           isCompleted={true}
-          value="H"
+          value="F"
           status="correct"
         />
-        <Cell value="A" isCompleted={true} />
-        <Cell value="P" isCompleted={true} />
-        <Cell value="P" isCompleted={true} />
-        <Cell value="Y" isCompleted={true} />
+        <Cell value="U" isCompleted={true} />
+        <Cell value="U" isCompleted={true} />
+        <Cell value="C" isCompleted={true} />
+        <Cell value="K" isCompleted={true} />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter H is in the word and in the correct spot.
+        The letter F is in the word and in the correct spot.
       </p>
 
-      <div className="mb-1 mt-4 flex justify-center">
-        <Cell value="B" isCompleted={true} />
-        <Cell value="I" isCompleted={true} />
-        <Cell
-          isRevealing={true}
-          isCompleted={true}
-          value="R"
-          status="present"
-        />
-        <Cell value="T" isCompleted={true} />
-        <Cell value="H" isCompleted={true} />
-      </div>
-      <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter R is in the word but in the wrong spot.
-      </p>
-
-      <div className="mb-1 mt-4 flex justify-center">
-        <Cell value="P" isCompleted={true} />
-        <Cell value="A" isCompleted={true} />
-        <Cell value="R" isCompleted={true} />
-        <Cell isRevealing={true} isCompleted={true} value="T" status="absent" />
-        <Cell value="Y" isCompleted={true} />
-      </div>
-      <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter T is not in the word in any spot.
-      </p>
     </BaseModal>
   )
 }
